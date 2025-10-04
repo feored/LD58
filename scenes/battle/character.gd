aee4cb9e38d9b814a05485afad7c07b5e87e2615
feored <feordo@gmail.com>
2025-10-04 02:13:22 +0200

    add enemy centeR

--- scenes/battle/character.gd
+++ scenes/battle/character.gd
@@ -15,5 +15,5 @@ func _physics_process(delta: float) -> void:
     elif Input.is_action_pressed("down"):
         self.global_position += (self.global_transform.basis.z  * delta * movement_speed)
     var mouse_pos = Utils.get_mouse_pos(get_viewport().get_camera_3d())
-    Log.info("Mouse pos: %s" % str(mouse_pos))
+    # Log.info("Mouse pos: %s" % str(mouse_pos))
     self.look_at(Vector3(mouse_pos.x, self.global_position.y, mouse_pos.z))
