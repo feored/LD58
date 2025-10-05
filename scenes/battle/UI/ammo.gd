5caabee26704fbf03a016fd312442b6f0ed983a2
feored <feordo@gmail.com>
2025-10-05 04:26:34 +0200

    add fonts and fix ammo order

--- scenes/battle/UI/ammo.gd
+++ scenes/battle/UI/ammo.gd
@@ -10,24 +10,24 @@ func _ready():
     inventory.updated.connect(_on_inventory_updated)
     _on_inventory_updated()
 
+func clear_skulls():
+    for skull in skulls:
+        skull.queue_free()
+    skulls.clear()
+
 func _on_inventory_updated():
     var item_count = inventory.items.size()
-    if skulls.size() < item_count:
-        for i in range(item_count - skulls.size()):
-            var new_skull = skull_icon.instantiate()
-            add_child(new_skull)
-            skulls.append(new_skull)
-    elif skulls.size() > item_count:
-        for i in range(skulls.size() - item_count):
-            var skull_to_remove = skulls.pop_back()
-            remove_child(skull_to_remove)
-            skull_to_remove.queue_free()
+    clear_skulls()
     for i in range(item_count):
-        var item = inventory.items[i]
-        match item.item_type:
-            Item.Type.Red:
-                skulls[i].self_modulate = Color(1, 0.19, 0.19, 1)
-            Item.Type.Blue:
-                skulls[i].self_modulate = Color(0.19, 0.39, 1, 1)
-            Item.Type.Green:
-                skulls[i].self_modulate = Color(0.19, 1, 0.39, 1)
+        var skull_instance = skull_icon.instantiate() as TextureRect
+        add_child(skull_instance)
+        skulls.append(skull_instance)
+    var blue_count = inventory.count_items_of_type(Item.Type.Blue)
+    var green_count = inventory.count_items_of_type(Item.Type.Green)
+    var red_count = inventory.count_items_of_type(Item.Type.Red)
+    for i in range(blue_count):
+        skulls[i].modulate = Color(0.2, 0.2, 1.0)
+    for i in range(blue_count, blue_count + green_count):
+        skulls[i].modulate = Color(0.2, 1.0, 0.2)
+    for i in range(blue_count + green_count, item_count):
+        skulls[i].modulate = Color(1.0, 0.2, 0.2)
